1a92a7f49085c7d63e73811e011a78bda75d8250
Kelly Kitui <kituikelly@gmail.com>
2024-03-10 13:54:43 +0300

    Create Looping functions, for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+/* writeCards()
+      ✓ returns an array of thank you messages for each name provided to the function */
+    
+let messages=[];
+
+function writeCards(stringNames, eventName){
+   for (let i = 0; i<stringNames.length; i++){
+      messages.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`);
+   }
+   return messages;
+}
+writeCards();
+
+
+/* countDown()    ✓ invokes console.log once for each number, counting down from the number provided to zero      ✓ logs each number when counting down, starting from the number provide */
+function countDown(positiveInteger){
+let i = 0;
+while(i <= positiveInteger){
+      console.log(positiveInteger--)
+}
+}
+countDown()
